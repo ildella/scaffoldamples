0ab59880f924064b9245b79fda4ba2a28aca969c
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2020-11-17 15:55:40 +0100

    now apply is succesfull, but something is missing

--- deploy-postcode-matrix.sh
+++ deploy-postcode-matrix.sh
@@ -12,6 +12,22 @@ aws s3 cp "$appName".zip s3://scaffoldamples-serverless/v"$version"/"$appName".z
 rm "$appName".zip
 cd ..
 
-cd infrastructure
-terraform apply -var="app_version=$version"
-cd ..
+# cd infrastructure
+# terraform apply -var="app_version=$version"
+# cd ..
+
+# https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-awscli.html
+# This part is covered in basics.tf and the resulting arn:aws are in terraform.tfstate
+# aws iam create-role --role-name lambda-ex --assume-role-policy-document file://trust-policy.json
+# aws create-user
+
+region=eu-west-2
+apiId=postcode-matrix-api
+stage=dev
+stage=postcode-matrix-dev
+
+lambdaFunction=ScaffoldamplesPostcodeMatrix
+YOUR_ACCOUNT_ID=
+aws lambda add-permission --function-name arn:aws:lambda:XXXXXX:$lambdaFunction --source-arn arn:aws:execute-api:$region:$YOUR_ACCOUNT_ID:$apiId/*/HTTP_METHOD/resource --principal apigateway.amazonaws.com --statement-id apigateway-access --action lambda:InvokeFunction
+
+curl https://$apiId.execute-api.$region.amazonaws.com/$stage
--- infrastructure/postcode-matrix-http.tf
+++ infrastructure/postcode-matrix-http.tf
@@ -1,7 +1,17 @@
-# resource "aws_apigatewayv2_api" "postcode_matrix_gateway" {
-#   name          = "postcode-matrix-http-api"
-#   protocol_type = "HTTP"
-# }
+resource "aws_apigatewayv2_api" "postcode_matrix_gateway" {
+  name          = "postcode-matrix-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_route" "postcode_matrix_route" {
+  api_id    = aws_apigatewayv2_api.postcode_matrix_gateway.id
+  route_key = "$default"
+}
+
+resource "aws_apigatewayv2_stage" "postcode_matrix_stage" {
+  api_id = aws_apigatewayv2_api.postcode_matrix_gateway.id
+  name   = "postcode-matrix-dev"
+}
 
 # resource "aws_apigatewayv2_integration" "postcode_matrix_integration" {
 #   api_id             = aws_apigatewayv2_api.postcode_matrix_gateway.id
@@ -10,13 +20,14 @@
 #   integration_uri    = "aws_lambda_function.postcode_matrix.invoke_arn"
 # }
 
-# resource "aws_apigatewayv2_route" "postcode_matrix_route" {
-#   api_id    = aws_apigatewayv2_api.postcode_matrix_gateway.id
-#   route_key = "$default"
-# }
+resource "aws_apigatewayv2_integration" "postcode_matrix_integration" {
+  api_id           = aws_apigatewayv2_api.postcode_matrix_gateway.id
+  integration_type = "AWS_PROXY"
 
-
-# resource "aws_apigatewayv2_stage" "postcode_matrix_stage" {
-#   api_id = aws_apigatewayv2_api.postcode_matrix_gateway.id
-#   name   = "postcode-matrix-dev"
-# }
+  connection_type           = "INTERNET"
+  # content_handling_strategy = "CONVERT_TO_TEXT"
+  description               = "Lambda example"
+  integration_method        = "POST"
+  integration_uri           = aws_lambda_function.postcode_matrix.invoke_arn
+  passthrough_behavior      = "WHEN_NO_MATCH"
+}
